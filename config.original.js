b88c0c8da91b87fade8f7b77ca93fee20dda37e6
yanxi-com <blogbbs@gmail.com>
2013-06-07 01:17:22 +0800

    rename config.js for deploy easy

--- config.original.js
+++ config.original.js
@@ -10,7 +10,7 @@ var config = {
     mongodb: "mongodb://localhost/qiri?poolSize=10",
     cookieSecret: "your secret for cookie",
     pwdSecret: "your secret for pwd",
-    defaultPage: "" // 目前通过站点无法创建公开的页面，只能通过手工更新 mongodb，设置 page.rootId 为空来创建
+    defaultPage: "" // 目前只能通过手工更新 mongodb，设置 page.rootId 为空来创建公开的页面
 };
 
 exports.get = function(key) {
