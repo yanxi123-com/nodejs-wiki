614c52caea8fa307425a79fa890d3b63bd979d53
yanxi-com <blogbbs@gmail.com>
2013-06-11 22:56:10 +0800

    add model/qiri-utils.js

---
+++ model/qiri-utils.js
@@ -0,0 +1,22 @@
+
+/*
+ * 七日知识管理系统
+ * Copyright(c) qiri.com <yanxi@yanxi.com>
+ * MIT Licensed
+ */
+ 
+var _ = require('underscore');
+
+exports.ready = function() {
+  return _(arguments).every(function(arg, index) {
+    var isDefined = ! _.isUndefined(arg);
+    if (0) {
+      console.log("arguments[" + index + "]:");
+      console.log("    arg=" + arg)
+      console.log("    typeof=" + typeof arg)
+      console.log("    isDefined=" + isDefined)
+    }
+    return isDefined;
+  });
+};
+
